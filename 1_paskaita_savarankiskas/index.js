ffc09d99b09624d4a9e0908bf67fb216052ca5dc
LabaiPiktas <121252533+LabaiPiktas@users.noreply.github.com>
2023-04-13 22:19:14 +0300

    kol kas tiek

---
+++ 1_paskaita_savarankiskas/index.js
@@ -0,0 +1,48 @@
+console.log("Hello savarankiskas darbas");
+const casual = require ('casual');
+const express = require("express");
+const app = express(); 
+const port = 3000;
+
+app.get("/randomUser", (req, res) => {
+    // req - request(kas ateina is isores), res - response(kas ateina is vidaus)
+    req.get("casual");
+    res.send(` ${casual.first_name}  ${casual.last_name}  ${casual.country}  ${casual.city}  ${casual.address}  `);  // send metodas issiuncia duomenis
+});
+
+app.get("/randomColor", (req, res) => {
+    req.get("casual");
+    res.send(casual.rgb_hex);
+});
+
+app.get("/randomColors", (req, res) => {
+    req.get("casual");
+    res.send(casual.rgb_array);
+});
+
+app.get("/randomColorsArr", (req, res) => {
+    const randomColorArray = [];
+    for(let i = 0; i < 5; i++){
+        randomColorArray[i] = casual.color_name;
+    }
+    res.send(randomColorArray);
+});
+
+
+
+app.get("/randomPlaces", (req, res) => {
+    req.get("casual");
+    res.send(casual.rgb_array);
+});
+
+/*const name = `${casual.name_prefix} ${casual.first_name} ${casual.last_name}`;
+console.log(name);*/
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
