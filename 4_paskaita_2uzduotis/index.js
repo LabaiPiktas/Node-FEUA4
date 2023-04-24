3fee2b2d9febb95c650de47c04d905c9ab80a13a
LabaiPiktas <121252533+LabaiPiktas@users.noreply.github.com>
2023-04-24 18:24:23 +0300

    3 veikia

--- 4_paskaita_2uzduotis/index.js
+++ 4_paskaita_2uzduotis/index.js
@@ -16,19 +16,29 @@ app.get("/", (req, res) => {
     res.send(data);
     });
 
-// 2
+// 2 
+
 app.get("/items/:category", (req, res) => {
     const category = req.params.category;
     const filteredCategory = data.filter((client) => client.category.toLowerCase() === category.toLowerCase());
     res.send(filteredCategory);
 });
 
-// 3
+// 3  uzkomentuoti 1 ir 2, kad paleisti 3
+
 app.get('/items/:id', (req, res) => {
-    const id = +req.params.id;
-    const foundItem = items.find((item) => item.id === +id);
-    res.send(foundItem);
+    const id = req.params.id;
+    
+    const foundItem = data.find((item) => item.id === +id);
+    if (foundItem) {
+        res.send(foundItem);
+    } else {
+        res.status(404).send('Item not found');
+    }
+    console.log(foundItem);
   });
+  
+  
  // 4
  app.get("/items", (req, res) => {
     const items = data.map((client) => client.name);
