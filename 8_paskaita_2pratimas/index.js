f42219761a8510bc85fa837080cc3d87a36f9832
LabaiPiktas <121252533+LabaiPiktas@users.noreply.github.com>
2023-05-03 21:51:25 +0300

    8 pratimai padaryti

---
+++ 8_paskaita_2pratimas/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const port = process.env.PORT || 8080;
+const URI = process.env.DB_CONNECTION_STRING;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const client = new MongoClient(URI);
+
+app.get("/movies", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con
+      .db("ManoDoumenuBaze")
+      .collection("Movies")
+      .find()
+      .toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post("/movies", async (req, res) => {
+  try {
+    const restaurant = req.body;
+    const con = await client.connect();
+    const data = await con
+      .db("ManoDoumenuBaze")
+      .collection("Movies")
+      .insertOne(restaurant);
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on the ${port} port`);
+});
