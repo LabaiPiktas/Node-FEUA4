20ceba3c45afddb4dd02a3c796a50a8146d38366
LabaiPiktas <121252533+LabaiPiktas@users.noreply.github.com>
2023-05-15 19:04:31 +0300

    NodeMongoDbFinalWorkPabaigtas - viskas veikia

---
+++ NodeMongoDbFinalWorkPabaigtas/client/script.js
@@ -0,0 +1,101 @@
+const membershipsList = document.getElementById('memberships');
+const addMembershipForm = document.getElementById('addMembershipForm');
+
+// Function to fetch the memberships and display them on the page
+async function fetchMemberships() {
+  const response = await fetch('http://localhost:3000/memberships');
+  const memberships = await response.json();
+
+  membershipsList.innerHTML = '';
+  memberships.forEach((membership) => {
+    const box = document.createElement('div');
+    box.classList.add('membership-box');
+
+    const priceAndName = document.createElement('div');
+    priceAndName.classList.add('price-and-name');
+
+    const price = document.createElement('p');
+    price.classList.add('membership-price');
+    price.textContent = '$' + membership.price;
+    priceAndName.appendChild(price);
+
+    const name = document.createElement('h3');
+    name.classList.add('membership-name');
+    name.textContent = membership.name;
+    priceAndName.appendChild(name);
+
+    box.appendChild(priceAndName);
+
+    const description = document.createElement('p');
+    description.classList.add('membership-description');
+    description.textContent = membership.description;
+    box.appendChild(description);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      deleteMembership(membership._id);
+    });
+    box.appendChild(deleteButton);
+
+    membershipsList.appendChild(box);
+  });
+}
+
+// Call fetchMemberships function to display the memberships when the page loads
+fetchMemberships();
+
+// Function to handle form submission and add a new membership
+async function addMembership(event) {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const price = document.getElementById('price').value;
+  const description = document.getElementById('description').value;
+
+  const response = await fetch('http://localhost:3000/memberships', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, price, description }),
+  });
+
+  const data = await response.json();
+
+  // Refresh the memberships list after adding a new membership
+  await fetchMemberships();
+
+  // Clear the form fields after adding a new membership
+  addMembershipForm.reset();
+}
+
+// Function to delete a membership
+// Change the parameter name of deleteMembership() to membershipId
+async function deleteMembership(membershipId) {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/memberships/${membershipId}`,
+      {
+        method: 'DELETE',
+      },
+    );
+    const data = await response.json();
+    if (response.status === 404) {
+      alert('Membership not found');
+    } else {
+      await fetchMemberships();
+      console.log(data);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Modify the delete button click event to pass membership._id as the parameter
+deleteButton.addEventListener('click', () => {
+  deleteMembership(membership._id);
+});
+
+// Add event listener to handle form submission and add a new membership
+addMembershipForm.addEventListener('submit', addMembership);
---
+++ NodeMongoDbFinalWorkPabaigtas/index.js
@@ -0,0 +1,211 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ObjectId } = require('mongodb');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 8080;
+const URI = process.env.DB_CONNECTION_STRING;
+const dbName = process.env.DB_NAME;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const client = new MongoClient(URI);
+// 1
+app.get('/memberships', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db(dbName).collection('services').find().toArray();
+    await con.close();
+    if (data.length === 0) {
+      res.status(404).send('No membership found');
+    } else {
+      res.send(data);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+//  2
+app.post('/memberships', async (req, res) => {
+  try {
+    const membership = req.body;
+    const con = await client.connect();
+    const existingMembership = await con
+      .db(dbName)
+      .collection('services')
+      .findOne({ name: membership.name });
+    if (existingMembership) {
+      res.status(409).send({ error: 'Membership already exists' });
+    } else {
+      const data = await con
+        .db(dbName)
+        .collection('services')
+        .insertOne(membership);
+      await con.close();
+      res.send(data);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+// 3
+app.delete('/memberships/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const con = await client.connect();
+    const data = await con
+      .db(dbName)
+      .collection('services')
+      .deleteOne({ _id: new ObjectId(id) }); // ištrina vieną
+    await con.close();
+    if (data.deletedCount === 0) {
+      res.status(404).send('Membership not found');
+    } else {
+      res.send(data);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+// 4
+// asc - ascending - didėjimo tvarka
+// dsc - descending - mažėjimo tvarka
+
+app.get('/users/:order', async (req, res) => {
+  try {
+    const { order } = req.params;
+    const sort = order === 'asc' ? 1 : -1;
+    const con = await client.connect();
+    const data = await con
+      .db(dbName)
+      .collection('users')
+      .find()
+      .sort({ name: sort })
+      .toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+// 4
+/*app.get('/users/:order', async (req, res) => {
+  try {
+    const { order } = req.params;
+    const sort = order === 'asc' ? 1 : -1;
+    const con = await client.connect();
+    const users = await con
+      .db(dbName)
+      .collection('users')
+      .find()
+      .sort({ name: sort })
+      .toArray();
+
+    const membershipIds = users.map(user => ObjectId(user.membershipType));
+    const memberships = await con
+      .db(dbName)
+      .collection('memberships')
+      .find({ _id: { $in: membershipIds } })
+      .toArray();
+
+    const usersWithMemberships = users.map(user => {
+      const membership = memberships.find(m => m._id.toString() === user.membershipType);
+      return {
+        ...user,
+        membershipType: membership ? membership.name : 'Unknown',
+      };
+    });
+
+    await con.close();
+    res.send(usersWithMemberships);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});*/
+
+// 5
+app.post('/users', async (req, res) => {
+  try {
+    const user = req.body;
+    const con = await client.connect();
+
+    const existingUser = await con
+      .db(dbName)
+      .collection('users')
+      .findOne({ email: user.email });
+    if (existingUser) {
+      await con.close();
+      return res.status(400).send({ message: 'User already exists' });
+    }
+    const data = await con.db(dbName).collection('users').insertOne(user);
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+// users get
+app.get('/users', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db(dbName).collection('users').find().toArray();
+    await con.close();
+    if (data.length === 0) {
+      res.status(404).send('No users found');
+    } else {
+      res.send(data);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get('/', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db(dbName).collection('users').find().toArray();
+    await con.close();
+    if (data.length === 0) {
+      res.status(404).send('No users found');
+    } else {
+      res.send(data);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+// ...
+
+// ...
+
+// Fetch a single membership by ID
+app.get('/memberships/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const con = await client.connect();
+    const membership = await con
+      .db(dbName)
+      .collection('services')
+      .findOne({ _id: new ObjectId(id) });
+    await con.close();
+
+    if (membership) {
+      res.send(membership);
+    } else {
+      res.status(404).send('Membership not found');
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// ...
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on the ${port}`);
+});
